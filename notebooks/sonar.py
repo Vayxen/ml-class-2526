56c86d84babbb80f65b44fd712f95e27d078fc39
Vittorio Strano <vittostrano3302@gmail.com>
2026-03-30 20:49:34 +0200

    sonar cleanup

--- notebooks/sonar.py
+++ notebooks/sonar.py
@@ -5,24 +5,38 @@ app = marimo.App(auto_download=["ipynb"])
 
 with app.setup:
     import marimo as mo
+
+    # Calcolo e plotting
     import pandas as pd
-
-    # import numpy as np
     import random
-
-    # import numpy as np
     import matplotlib.pyplot as plt
     import seaborn as sns
 
+    # Scikit-learn
     from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-    from sklearn.preprocessing import StandardScaler  # , LabelEncoder
+    from sklearn.preprocessing import StandardScaler, LabelEncoder
 
     # from sklearn.neighbors import KNeighborsClassifier
     from sklearn.model_selection import train_test_split
 
 
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    # Dataset "sonar" e applicazione della LDA ad uno spazio di 60 feature
+
+    Le 60 feature corrispondono alle misure dei sensori su 60 bande di frequenza. Il dataset è stato importato dal link fornito, appendendo (puramente come scelta di stile) una riga di titoli. Le classi (etichette) sono rappresentate dalle lettere R (Roccia) e M (Mina) in base all'oggetto rilevato dalle misure del sonar.
+
+    Alla colonna delle etichette è stato applicato il `LabelEncoder` di `scikit-learn`, così da convertire le etichette, originariamente di tipo stringa, in valori numerici.
+
+    La *linear discriminant analysis* è qui applicata come classificatore, scalando prima i dati numerici con lo standard scaling.
+    """)
+    return
+
+
 @app.cell
 def _():
+    # Riga dei titoli
     titles = [f"Banda {number}" for number in range(0, 60)]
     titles.append("Oggetto")
 
@@ -32,10 +46,11 @@ def _():
         names=titles,
     )
 
+    # df["Oggetto"] = df["Oggetto"].replace({"R": 0, "M": 1}).infer_objects()
 
-    # Roccia = 0, Mina = 1
-
-    df["Oggetto"] = df["Oggetto"].replace({"R": 0, "M": 1})
+    # Quello sopra è un approccio manuale, tuttavia sklearn offre un metodo più diretto per occuparsi della codifica numerica di label di tipo letterale, come segue:
+    le = LabelEncoder()
+    df["Oggetto"] = le.fit_transform(df["Oggetto"])
 
     x = df.iloc[:, :-1]
     y = df["Oggetto"].values.ravel()
@@ -55,10 +70,12 @@ def _():
 @app.cell
 def _(x, y):
     seed = random.randint(0, 50)
+    # per testare la performance del modello a partire da vari stati iniziali
     fix_seed = 42
+    # per testare il funzionamento del codice
 
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.2, random_state=fix_seed, stratify=y
+        x, y, test_size=0.2, random_state=seed, stratify=y
     )
 
     lda = LinearDiscriminantAnalysis()
@@ -79,9 +96,8 @@ def _():
 
 @app.cell
 def _(lda, x_test_scaled, x_train_scaled, y_train):
-    y_pred = lda.fit(x_train_scaled, y_train).predict(
-        x_test_scaled
-    )  # il dataset di test è come un foglio di verifica
+    y_pred = lda.fit(x_train_scaled, y_train).predict(x_test_scaled)
+    # il dataset di test è come un foglio di verifica
     return (y_pred,)
 
 
@@ -136,10 +152,10 @@ def _(y_pred, y_test):
 def _(accuracy, f1, precision, recall):
     mo.md(rf"""
     Il modello appena eseguito ha restituito le seguenti metriche: 
-    - Accuratezza: ${round(accuracy, 3) * 100}\%$
-    - Precisione: ${round(precision, 3) * 100}\%$
-    - Recall: ${round(recall, 3) * 100}\%$
-    - F1-score: ${round(f1, 3) * 100}\%$
+    - Accuratezza: ${round(accuracy, 4) * 100}\%$
+    - Precisione: ${round(precision, 4) * 100}\%$
+    - Recall: ${round(recall, 4) * 100}\%$
+    - F1-score: ${round(f1, 4) * 100}\%$
     """)
     return
 
